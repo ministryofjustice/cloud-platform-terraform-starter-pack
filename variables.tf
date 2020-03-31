7b830c5285f43e7fce5f822a6fa0c7e07697039a
poornima-moj <poornima.krishnasamy@digital.justice.gov.uk>
2020-03-31 14:44:27 +0100

    Removing cluster related vars

--- variables.tf
+++ variables.tf
@@ -1,10 +1,3 @@
-variable "cluster_name" {
-  description = "The name of the cluster (eg.: test-1)"
-}
-
-variable "cluster_state_bucket" {
-  description = "The name of the S3 bucket holding the terraform state for the cluster"
-}
 
 variable "namespace" {
   default = "starter-pack"
